7480810172a1562c421e1285738664426e56690c
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-27 11:19:39 -0600

    Learning about classes and objects

--- public/lecture.php
+++ public/lecture.php
@@ -1,43 +1,32 @@
 <!doctype html>
 <html>
 <head>
-    <title>File Upload</title>
+    <title>Classes</title>
 </head>
 <body>
 
 <?php 
 
-	// Verify there were uploaded files and no errors
-if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
-    // Set the destination directory for uploads
-    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
-    // Grab the filename from the uploaded file by using basename
-    $filename = basename($_FILES['file1']['name']);
-    // Create the saved filename using the file's original name and our upload directory
-    $saved_filename = $upload_dir . $filename;
-    // Move the file from the temp location to our uploads directory
-    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+class Conversation {
+    // Property to hold name
+    public $name = '';
+    public $lastname = '';
+
+    // Method to say hello to name
+    function say_hello() {
+        return "Hello {$this->name} {$this->lastname}.";
+    }
 }
 
-// Check if we saved a file
-if (isset($saved_filename)) {
-    // If we did, show a link to the uploaded file
-    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
-}
+// Create a new instance of Conversation
+$chat = new Conversation();
 
+// Set the $name variable
+$chat->name = 'Codeup';
+$chat->lastname = 'Cohort';
+
+// Output greeting to $name
+echo $chat->say_hello();
 ?>
-
-<h1>Upload File</h1>
-
-<form method="POST" enctype="multipart/form-data">
-    <p>
-        <label for="file1">File to upload: </label>
-        <input type="file" id="file1" name="file1">
-    </p>
-    <p>
-        <input type="submit" value="Upload">
-    </p>
-</form>
-
 </body>
 </html>
