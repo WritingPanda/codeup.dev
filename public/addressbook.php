79a8849d5df3977ba81f965c883e88620fcfe9f9
WritingPanda <omar.quimbaya4@gmail.com>
2014-02-27 14:26:33 -0600

    Built remove feature, needing to build class and use it instead of functions

--- public/addressbook.php
+++ public/addressbook.php
@@ -4,18 +4,29 @@ $filename = 'data/addressbook.csv';
 $address_book = array();
 $entries = array();
 
-class ADdressDataStore {
+// class AddressDataStore {
 	
-	public $filename = '';
+// 	public $filename = '';
 
-	function readCSV() {
-		// Code to read file $this->filename
-	}
+// 	function readCSV() {
+// 		$contents = [];
+// 		$handle = fopen($this->filename, "r");
+// 		while(($data = fgetcsv($handle)) !== FALSE) {
+// 			$contents[] = $data;
+// 		}
+//     	fclose($handle);
+//     	return $contents;
+// 	}
 
-	function store_entry($addresses_array) {
-		// Code to write $addresses_array to file $this->filename
-	}
-}
+// 	function store_entry($rows) {
+// 		// Code to write $addresses_array to file $this->filename
+// 		$handle = fopen($this->filename, 'w');
+// 		foreach ($rows as $row) {
+// 			fputcsv($handle, $row);
+// 		}
+// 		fclose($handle);
+// 		}
+// }
 
 function store_entry($filename, $rows) {
 	$handle = fopen($filename, 'w');
@@ -35,19 +46,31 @@ function readCSV($filename) {
     return $contents;
 }
 
+// $adrbk = new AddressDataStore();
+// $adrbk->filename = 'data/addressbook.csv';
+
 $address_book = readCSV($filename);
+$errors = [];
 
 if (!empty($_POST)) {
-	$name = $_POST['name'];
-	$address = $_POST['address'];
-	$city = $_POST['city'];
-	$state = $_POST['state'];
-	$zip = $_POST['zip'];
-	$phone = $_POST['phone'];
-	$entries = [$name, $address, $city, $state, $zip, $phone];
-	array_push($address_book, $entries);
+	$entry = [];
+	$entry['name'] = $_POST['name'];
+	$entry['address'] = $_POST['address'];
+	$entry['city'] = $_POST['city'];
+	$entry['state'] = $_POST['state'];
+	$entry['zip'] = $_POST['zip'];
+	$entry['phone'] = $_POST['phone'];
+	foreach ($entry as $key => $value) {
+		if (empty($value)) {
+			echo "<p>{$key} is not defined.</p>";
+		} else{
+			$entry[] = $value;
+		}
+	array_push($address_book, $entry);
 	store_entry($filename, $address_book);
+	}
 }
+
 ?>
 
 <!doctype html>
@@ -78,38 +101,49 @@ padding:5px;
 			<th>State</th>
 			<th>Zip</th>
 			<th>Phone</th>
+			<th>Remove link</th>
 		</tr>
 		<?php 
 
-		foreach ($address_book as $entries) {
+		foreach ($address_book as $key => $entries) {
 			echo "<tr>";
 			foreach ($entries as $entry) {
-				echo "<td>" . $entry . "</td>";
+				echo "<td>" . htmlspecialchars(strip_tags($entry)) . "</td>";
 			}
+			echo "<td><a href=?remove={$key}>Remove Entry</a></td>";
+
 		}
 		echo "</tr>";
+
+		if (isset($_GET['remove'])) {
+			$key = $_GET['remove'];
+			unset($address_book[$key]);
+			store_entry('data/addressbook.csv', $address_book);
+			header('Location: addressbook.php');
+			exit(0);
+		}
 		?>
 	</table></center>
 	<center><p>Please fill out the fields to enter a new entry in your address book:</p>
 	<form method='POST' enctype='multipart/form-data' action='addressbook.php'>
 		<p>
 			<label for='name'>Name: </label>
-			<input id='name' name='name' type='text' autofocus='autofocus' required>
+			<input id='name' name='name' type='text' autofocus='autofocus'>
 
 			<label for='address'>Address: </label>
-			<input id='address' name='address' type='text' required>
+			<input id='address' name='address' type='text'>
 		
 			<label for='city'>City: </label>
-			<input id='city' name='city' type='text' required>
+			<input id='city' name='city' type='text'>
 		
 			<label for='state'>State: </label>
-			<input id='state' name='state' type='text' required>
+			<input id='state' name='state' type='text'>
 		
 			<label for='zip'>Zip: </label>
-			<input id='zip' name='zip' type='text' required>
+			<input id='zip' name='zip' type='text'>
 		
 			<label for='phone'>Phone: </label>
-			<input id='phone' name='phone' type='text' required>
+			<input id='phone' name='phone' type='text'>
 		
 			<button type='submit'>Add Address</button>
 		</p></center>
