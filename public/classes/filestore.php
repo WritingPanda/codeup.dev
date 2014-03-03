6620c1da169bef31044741fd107d12e7cc202619
WritingPanda <omar.quimbaya4@gmail.com>
2014-03-03 09:06:03 -0600

    Editing for consistency in code

--- public/classes/filestore.php
+++ public/classes/filestore.php
@@ -11,7 +11,7 @@ class Filestore {
 
     // Returns array of lines in $this->filename
     public function read_lines() {
-        $handle = fopen($this->filename, "r");
+        $handle = fopen($this->filename, 'r');
         $size = filesize($this->filename);
         $contents = fread($handle, $size);
         $contents_array = explode("\n", $contents);
@@ -30,7 +30,7 @@ class Filestore {
     // Reads contents of csv $this->filename, returns an array
     public function read_csv() {
         $contents = [];
-        $handle = fopen($this->filename, "r");
+        $handle = fopen($this->filename, 'r');
         while(($data = fgetcsv($handle)) !== FALSE) {
             $contents[] = $data;
         }
