38e444680cba14569fde3f3d410a2159908fb347
WritingPanda <omar.quimbaya4@gmail.com>
2014-03-05 14:22:35 -0600

    Changing lecture to move along with the class lecture

--- public/lecture.php
+++ public/lecture.php
@@ -1,48 +1,20 @@
-<?php 
-
-class Conversation {
-    // Property to hold name
-    public $name = '';
-    public $lastname = '';
-
-    // Method to say hello to name
-    function say_hello($paragraph = TRUE) {
-        $greeting = "Hello {$this->name} {$this->lastname}.";
-
-        if ($paragraph == TRUE) {
-            return "<p>$greeting</p>";
-        } else{
-            return $greeting . PHP_EOL;
-        }
-    }
-
-    // Method to say goodbye
-    function say_goodbye() {
-        return "Goodbye, {$this->name} {$this->lastname}!";
-    }
-
-    // Allow name to be set on instantiation
-    function __construct($name = '') {
-        $this->name = $name;
-    }
-
-    function __destruct() {
-        echo "Goodbye, {$this->name}\n";
-    }
-}
-
-?>
 <!doctype html>
 <html>
 <head>
-    <title>Classes</title>
+    <title>Dates in JavaScript</title>
 </head>
 <body>
-<?php 
+    <script src='/js/moment.js'></script>
+    <script>
+        var now = moment();
 
-$conversation = new Conversation();
+        // alert("The date is " + now.format("dddd, MMMM, Do YYYY, h:mm:ss a") + ".");
+        console.log(now.format("dddd, MMMM, Do YYYY, h:mm:ss a"));
 
+        console.log(moment().subtract('days', 1).fromNow());
 
-?>
+        var codeup = moment('2-4-2014', 'MM-DD-YYYY');
+        console.log(codeup.fromNow());
+    </script>
 </body>
 </html>
