92248167829f08ba0ecd62ce550b64fc1aadacbb
WritingPanda <omar.quimbaya4@gmail.com>
2014-03-05 20:52:05 -0600

    Refactoring to add divs in HTML to make it easier to style the website and app

--- public/todolist.php
+++ public/todolist.php
@@ -33,85 +33,97 @@ if (count($_FILES) > 0 && $_FILES['upload']['error'] == 0 && $_FILES['upload']['
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="/css/todoaddressbkstyle.css" type='text/css'>
+	<link rel="stylesheet" href="/css/todostyle.css" type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Coda+Caption:800|Bubbler+One' rel='stylesheet' type='text/css'>
 	<title>Todo List</title>
 </head>
 <body>
-	<h1>TODO List</h1>
-	<img src='img/do-all-things.png' width=200 height=150 class='img-rounded'>
-	<br><br>
-	<form method='POST' action='todolist.php'>
-	<ul>
-		<?php 
+	<div>
+		<h1>TODO List</h1>
+		<img src='img/do-all-things.png' width=200 height=150 class='img-rounded'>
+	</div>
+	<br>
+	<div class='newitem'>
+		<form method='POST' action='todolist.php'>
+		<ul>
+			<?php 
 
-		$items = $todo->read();
-		try{
-			if (isset($_POST['newitem']) && empty($_POST['newitem'])) {
-				throw new InvalidInputException('No tasks were entered in the todo list. <strong>Please enter a task.</strong>');
+			$items = $todo->read();
+
+			try{
+				if (isset($_POST['newitem']) && empty($_POST['newitem'])) {
+					throw new InvalidInputException('No tasks were entered in the todo list. <strong>Please enter a task.</strong>');
+				}
+
+				if (isset($_POST['newitem']) && strlen($_POST['newitem']) > 240) {
+					throw new InvalidInputException('Task is longer than 240 characters. <strong>Please make it shorter.</strong>');
+				}
+				if (!empty($_POST['newitem'])) {
+					$newItem = $_POST['newitem'];
+					array_push($items, $newItem);
+					$todo->write($items);
+					header('Location: todolist.php');
+					exit(0);
+				}
+			} catch (InvalidInputException $e) {
+				echo $e->getMessage();
 			}
 
-			if (isset($_POST['newitem']) && strlen($_POST['newitem']) > 240) {
-				throw new InvalidInputException('Task is longer than 240 characters. <strong>Please make it shorter.</strong>');
+			if (!empty($_FILES['upload']) && $_FILES['upload']['type'] == 'text/plain') {
+				$todo->filename = "/vagrant/sites/codeup.dev/public/uploads/{$Newfilename}";
+				$newFileArray = $todo->read();
+				$combineArray = array_merge($items, $newFileArray);
+				$todo->filename = 'data/todo_list.txt';
+				$todo->write($combineArray);
+				header('Location: todolist.php');
+				exit(0);
+			} elseif (count($_FILES) > 0 && $_FILES['upload']['type'] != 'text/plain') {
+				echo "<p><strong>ERROR:</strong> File is not a txt file.</p>";
 			}
-			if (!empty($_POST['newitem'])) {
-				$newItem = $_POST['newitem'];
-				array_push($items, $newItem);
+
+			foreach ($items as $key => $item) {
+				echo "<li>" . htmlspecialchars(strip_tags($item)) . " | <a href='?remove={$key}'>Complete</a></li>";
+			
+			}
+
+			if (isset($_GET['remove'])) {
+				$key = $_GET['remove'];
+				unset($items[$key]);
 				$todo->write($items);
 				header('Location: todolist.php');
 				exit(0);
 			}
-		} catch (InvalidInputException $e) {
-			echo $e->getMessage();
-		}
-
-		if (!empty($_FILES['upload']) && $_FILES['upload']['type'] == 'text/plain') {
-			$todo->filename = "/vagrant/sites/codeup.dev/public/uploads/{$Newfilename}";
-			$newFileArray = $todo->read();
-			$combineArray = array_merge($items, $newFileArray);
-			$todo->filename = 'data/todo_list.txt';
-			$todo->write($combineArray);
-			header('Location: todolist.php');
-			exit(0);
-		} elseif (count($_FILES) > 0 && $_FILES['upload']['type'] != 'text/plain') {
-			echo "<p><strong>ERROR:</strong> File is not a txt file.</p>";
-		}
-
-		foreach ($items as $key => $item) {
-			echo "<li>" . htmlspecialchars(strip_tags($item)) . " | <a href='?remove={$key}'>Complete</a></li>";
-		
-		}
-
-		if (isset($_GET['remove'])) {
-			$key = $_GET['remove'];
-			unset($items[$key]);
-			$todo->write($items);
-			header('Location: todolist.php');
-			exit(0);
-		}
-		
-		?>
-	</ul>
-		<p>
-			<label for='newitem'>Task: </label>
-			<input id='newitem' name='newitem' type='text' placeholder='Enter task' autofocus='autofocus'>
-		</p>
-		<p>
-			<button class="btn btn-primary" type="submit">Add todo</button>
-		</p>
-	</form>
-	<form method='POST' enctype='multipart/form-data' action='todolist.php'>
-		<p>
-			<label for='upload'>Upload a file to add to the list: </label>
-			<input id='upload' name='upload' type='file'>
-		</p>
-		<p>
-			<button class="btn btn-default btn-lg" type="submit">Upload</button>
-		</p>
-
-	</form>
-	<footer>
-		<p style="font-family:Courier">&copy; 2014 <a href="http://writtenbyapanda.tumblr.com" target="_blank">Written by a Panda</a></p>
-	</footer>
+			
+			?>
+		</ul>
+		<br>
+			<p>
+				<label for='newitem'>Task: </label>
+				<input id='newitem' name='newitem' type='text' placeholder='Enter task' autofocus='autofocus'>
+			</p>
+			<br>
+			<p>
+				<button class="btn btn-primary" type="submit">Add todo</button>
+			</p>
+			<br>
+		</form>
+	</div>
+	<div class='upload'>
+		<form method='POST' enctype='multipart/form-data' action='todolist.php'>
+			<p>
+				<label for='upload'>Upload a file to add to the list: </label>
+				<input id='upload' name='upload' type='file'>
+			</p>
+			<p>
+				<button class="btn btn-default" type="submit">Upload</button>
+			</p>
+			<br>
+		</form>
+	</div>
+	<div>
+		<footer>
+			<p style="font-family:Courier">&copy; 2014 <a href="http://writtenbyapanda.tumblr.com" target="_blank">Written by a Panda</a></p>
+		</footer>
+	</div>
 </body>
 </html>
