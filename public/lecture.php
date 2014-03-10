206e7c797eaa686d5e876b8d2c44627ac5870fdb
WritingPanda <omar.quimbaya4@gmail.com>
2014-03-09 20:33:38 -0500

    Moving around resources

--- public/lecture.php
+++ public/lecture.php
@@ -1,20 +1,49 @@
 <!doctype html>
 <html>
 <head>
-    <title>Dates in JavaScript</title>
-</head>
-<body>
-    <script src='/js/moment.js'></script>
-    <script>
-        var now = moment();
-
-        // alert("The date is " + now.format("dddd, MMMM, Do YYYY, h:mm:ss a") + ".");
-        console.log(now.format("dddd, MMMM, Do YYYY, h:mm:ss a"));
-
-        console.log(moment().subtract('days', 1).fromNow());
-
-        var codeup = moment('2-4-2014', 'MM-DD-YYYY');
-        console.log(codeup.fromNow());
-    </script>
+    <title>CSS Float + Some JavaScript from a previous lesson</title>
+    <style>
+      #container {
+        margin: 50px auto;
+        width: 500px;
+        height: 200px;
+        border: 1px solid #000;
+        position: relative;
+      }
+      .box {
+        background-color: #1969B2;
+        height: 100px;
+        width: 100px;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+      }
+      div {
+        color: #FFF;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 100px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="container">
+      <div class="box">Stamp</div>
+    </div>
+    <p>
+        Bacon ipsum dolor sit amet frankfurter venison strip steak tongue chuck. Fatback tongue pig, beef meatball brisket pork chop jowl short ribs andouille bresaola ham hock ham filet mignon. Meatball short loin beef ribs rump pig, ham capicola pork strip steak chuck. Short ribs pork shankle beef ribs ham hock porchetta. Drumstick ribeye pork loin turducken porchetta prosciutto boudin. Fatback pork belly porchetta tenderloin leberkas andouille pork, flank sirloin tongue shankle. Pork belly capicola fatback short ribs porchetta, bacon prosciutto pork chop ham hock swine andouille beef landjaeger.
+    </p>
+    <p>
+        T-bone filet mignon tail tongue tri-tip. Jowl beef ribs tenderloin, ball tip tongue chuck salami pork bacon capicola. Shoulder biltong pork loin ground round, filet mignon tongue shankle landjaeger meatball pig capicola jerky pork. Frankfurter swine ground round meatball turkey ham. Chicken bresaola beef ribs, shank short loin biltong ground round chuck.
+    </p>
+    <p>
+        Kielbasa fatback ham tongue bacon jowl brisket landjaeger. Pork chop tenderloin biltong meatloaf. Ball tip doner frankfurter pork belly beef ribs shank pancetta filet mignon short ribs turducken jerky. Pork filet mignon beef leberkas pancetta drumstick meatloaf landjaeger ground round andouille tongue jerky. Sausage pastrami bacon pork loin ham sirloin. Pork belly corned beef frankfurter meatball turkey sirloin.
+    </p>
+    <p>
+        Ham andouille pig spare ribs swine doner. Meatball beef filet mignon turkey kevin, pork loin jerky sirloin. Jerky pork loin shank prosciutto pastrami, boudin meatball pork chop turkey pancetta shankle tri-tip drumstick. Pork belly filet mignon pork loin sausage pancetta pastrami t-bone beef pig leberkas bacon shank pork chop venison.
+    </p>
+    <p>
+        Pig sausage tri-tip tenderloin pork belly chicken pastrami short ribs kielbasa turducken flank. Swine sirloin jowl, shank frankfurter bresaola chuck leberkas shoulder rump ham hock pancetta fatback cow tongue. Ball tip pancetta tail, swine turducken pork chop ribeye tenderloin sirloin landjaeger rump. Pork chop sirloin salami corned beef t-bone pastrami pork loin shank capicola hamburger prosciutto venison bresaola ground round ribeye. Chicken ham hock tri-tip turkey venison turducken.
+    </p>
 </body>
 </html>
